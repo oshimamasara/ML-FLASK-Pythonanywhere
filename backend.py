0847ac10e81022943044876fc72e0a2d0ae5b611
oshimamasara <oshimamasara@gmail.com>
2019-11-16 12:50:52 +0900

    Update backend.py
    delete comment

--- backend.py
+++ backend.py
@@ -9,18 +9,3 @@ def days_feet(day):
     test_data = poly_reg.fit_transform([[day]])
     output = model.predict(test_data)
     return int(output)
-
-#model = pickle.load(open('model.pkl','rb'))
-#
-#def meters_feet(meter, coefficient = 3.28084):
-#    prediction = model.predict([[np.array(data[meter])]])
-#    output = prediction[0]
-#    return output
-    #return meters * coefficient
-
-
-#def predict():
-#    data = request.get_json(force=True)
-#    prediction = model.predict([[np.array(data['exp'])]])
-#    output = prediction[0]
-#    return jsonify(output)
