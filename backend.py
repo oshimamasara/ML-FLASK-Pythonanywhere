83b0d207822886252a162b5756f87d7553d968aa
oshimamasara <oshimamasara@gmail.com>
2019-11-16 14:49:02 +0900

    Update backend.py
    OK, Read .pkl on Pythonanywhere
    https://help.pythonanywhere.com/pages/NoSuchFileOrDirectory/

--- backend.py
+++ backend.py
@@ -1,10 +1,13 @@
-#import numpy as np
-#from flask import Flask, request, jsonify
 import pickle
 from sklearn.preprocessing import PolynomialFeatures
+import os
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+saved_model = os.path.join(THIS_FOLDER, 'model3.pkl')
 
 def days_feet(day):
-    model = pickle.load(open('model3.pkl','rb'))
+    #model = pickle.load(open('model3.pkl','rb'))
+    model = pickle.load(open(saved_model,'rb'))
     poly_reg = PolynomialFeatures(degree = 6)
     test_data = poly_reg.fit_transform([[day]])
     output = model.predict(test_data)
